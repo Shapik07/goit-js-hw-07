46d7eb0633bf4912b5214bfd3e5855ab6c4ab6de
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-28 16:09:24 +0300

    Update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const cardsMarkup = createImagesMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", cardsMarkup);
+
+gallery.addEventListener("click", onGalleryImageClick);
+
+function createImagesMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<img class="image" src="${preview}" alt="${description}" width="360" height="230"></img>`;
+    })
+    .join("");
+}
+
+function onGalleryImageClick(event) {
+    const isImageInGalleryElement = event.target.classList.contains("image");
+    if (!isImageInGalleryElement) {
+        return
+    };
+}
