3733494e61efb2d5e3089fed988b39c3ff1cf059
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-29 16:23:10 +0300

    Update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const markupGallery = createImagesMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", markupGallery);
+
+function createImagesMarkup() {
+    return galleryItems.map(({
+      preview: smallPicture,
+      original: bigPicture,
+      description,
+    }) => {
+        return `<a class="gallery__item" href="${bigPicture}">
+  <img class="gallery__image" src="${smallPicture}" alt="${description}" />
+</a>`;
+    }).join('');
+}
